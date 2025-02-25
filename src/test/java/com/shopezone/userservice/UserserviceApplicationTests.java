b006244ec7764ed0db6d1614347fe4a5e41fca48
GibrannReyesTR <giresa.ico@gmail.com>
2025-02-25 14:02:20 -0600

    missing files, project creation

---
+++ src/test/java/com/shopezone/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopezone.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
